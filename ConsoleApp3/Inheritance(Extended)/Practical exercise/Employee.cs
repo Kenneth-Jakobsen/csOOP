a195225443c79b2873e389e567106b69bc97513c
Kenneth Jakobsen <jakobsenkenneth@hotmail.no>
2025-03-09 14:28:58 +0100

    x

---
+++ ConsoleApp3/Inheritance(Extended)/Practical exercise/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Inheritance_Extended_.Practical_exercise
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public int EmployeeID { get; private set; }
+        public string Department { get; private set; }
+
+        public Employee(string name, int employeeID, string department)
+        {
+            Name = name;
+            EmployeeID = employeeID;
+            Department = department;
+        }
+
+        public virtual void Work()
+        {
+            Console.WriteLine("Working hard");
+        }
+    }
+}
